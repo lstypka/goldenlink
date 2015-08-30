ffe3bca45e7433ea95ceba9b18bcc96f591d5297
lstypka <lukasz.stypka@gmail.com>
2015-08-30 18:07:27 +0200

    client setup

---
+++ client/app/scripts/app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('clientApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/links', {
+                templateUrl: 'views/links.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/photos', {
+                templateUrl: 'views/photos.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/videos', {
+                templateUrl: 'views/videos.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/youtube', {
+                templateUrl: 'views/youtube.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/notes', {
+                templateUrl: 'views/notes.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/search', {
+                templateUrl: 'views/search.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
