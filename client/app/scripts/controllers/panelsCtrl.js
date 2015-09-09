369923e84d136d6c116cba838d986e6b6fe54f3d
Lukasz Stypka <lstypka@future-processing.com>
2015-09-09 16:34:55 +0200

    Added additional panels colours

--- client/app/scripts/controllers/panelsCtrl.js
+++ client/app/scripts/controllers/panelsCtrl.js
@@ -14,7 +14,7 @@ app.controller('panelsCtrl', ['$scope', 'close', function ($scope, close) {
 
 
     $scope.panels = ["panel-blue", "panel-red", "panel-green", "panel-orange", "panel-yellow",
-        "panel-lavender", "panel-olivedrab", "panel-khaki"];
+        "panel-lavender", "panel-olivedrab", "panel-khaki", "panel-purple", "panel-grey", "panel-blue-grey", "panel-pink", "panel-indigo", "panel-green-lighten", "panel-green-darken", "panel-lime"];
 
     var init = function () {
 
